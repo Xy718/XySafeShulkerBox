d3854d5c5a25cbc19a424b683e5f81feac343a1b
Xy718 <869839000@qq.com>
2020-09-07 08:41:22 +0800

    完工，已支持IronChest

--- src/main/java/xyz/xy718/safeshulker/event/BoxEvent.java
+++ src/main/java/xyz/xy718/safeshulker/event/BoxEvent.java
@@ -3,9 +3,11 @@ package xyz.xy718.safeshulker.event;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.slf4j.Logger;
+import org.spongepowered.api.block.tileentity.TileEntity;
 import org.spongepowered.api.block.tileentity.TileEntityTypes;
 import org.spongepowered.api.block.tileentity.carrier.ShulkerBox;
 import org.spongepowered.api.entity.living.player.Player;
@@ -18,8 +20,16 @@ import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.item.inventory.InteractInventoryEvent;
 import org.spongepowered.api.text.Text;
 
+import org.spongepowered.api.world.Location;
 import xyz.xy718.safeshulker.XySafeShulkerBoxPlugin;
 
+/**
+ * 事件类
+ * 诶呀，迟来的注释
+ * @author: Xy718
+ * @create: 2020/9/6
+ * @description:
+ */
 public class BoxEvent {
 
 	public static final Logger LOGGER = XySafeShulkerBoxPlugin.LOGGER;
@@ -31,10 +41,10 @@ public class BoxEvent {
 			InteractInventoryEvent.Open event
 			,@First Player player
 			) {
-		ShulkerBox sb=getShunkerOrNull(event);
-		if(sb!=null) {
+		Integer sbhashCode=getShunkerOrNull(event);
+		if(sbhashCode!=null) {
 			openedShulker.put(
-					sb.hashCode()
+					sbhashCode
 					,player.getUniqueId()
 					);
 		}
@@ -59,35 +69,52 @@ public class BoxEvent {
 			InteractBlockEvent event
 			,@First Player player
 			) {
-		event.getTargetBlock().getLocation().ifPresent( location ->{
-			location.getTileEntity().ifPresent( te ->{
+		if(event.getTargetBlock().getLocation().isPresent()){
+			Location location = event.getTargetBlock().getLocation().get();
+			if(location.getTileEntity().isPresent()){
+				TileEntity te= (TileEntity) location.getTileEntity().get();
+				LOGGER.info(String.valueOf(te));
 				//判断是否是原生sb
 				if(te.getType().equals(TileEntityTypes.SHULKER_BOX)) {
 					//是原生的sb，那么保存了吗？
-					if(openedShulker.containsKey(((ShulkerBox)te).hashCode())) {
+					if(openedShulker.containsKey(te.hashCode())) {
 						player.sendMessage(Text.of("其他人在使用"));
 						event.setCancelled(true);
 					}
-				}else if(te instanceof TileEntityIronShulkerBox){
+				}else if(te.getClass().getName().contains("cpw.mods.ironchest")){
 					//哦吼，是更多箱子的sb哦
-				}
-			} );
-		});
-
-	}
-	
-	ShulkerBox getShunkerOrNull(Event event){
-		ShulkerBox retS=null;
-		//死亡姨夫
-		if(event.getContext().get(EventContextKeys.BLOCK_HIT).isPresent()) {
-			if(event.getContext().get(EventContextKeys.BLOCK_HIT).get().getLocation().isPresent()) {
-				if(event.getContext().get(EventContextKeys.BLOCK_HIT).get().getLocation().get().getTileEntity().isPresent()) {
-					if(event.getContext().get(EventContextKeys.BLOCK_HIT).get().getLocation().get().getTileEntity().get().getType().equals(TileEntityTypes.SHULKER_BOX)) {
-						retS = (ShulkerBox) event.getContext().get(EventContextKeys.BLOCK_HIT).get().getLocation().get().getTileEntity().get();
+					if(te.getClass().getSimpleName().contains("ShulkerBox")){
+						if(openedShulker.containsKey(te.hashCode())) {
+							player.sendMessage(Text.of("其他人在使用"));
+							event.setCancelled(true);
+						}
 					}
 				}
 			}
 		}
-		return retS;
+
+	}
+
+	Integer getShunkerOrNull(Event event){
+		Integer retHashCode=null;
+		//死亡姨夫
+		if(event.getContext().get(EventContextKeys.BLOCK_HIT).isPresent()) {
+			if(event.getContext().get(EventContextKeys.BLOCK_HIT).get().getLocation().isPresent()) {
+				if(event.getContext().get(EventContextKeys.BLOCK_HIT).get().getLocation().get().getTileEntity().isPresent()) {
+					TileEntity te=event.getContext().get(EventContextKeys.BLOCK_HIT).get()
+							.getLocation().get()
+							.getTileEntity().get();
+					if(te.getType().equals(TileEntityTypes.SHULKER_BOX)) {
+						retHashCode = event.getContext().get(EventContextKeys.BLOCK_HIT).get().getLocation().get().getTileEntity().get().hashCode();
+					}else if(te.getClass().getName().contains("cpw.mods.ironchest")){
+						//哦吼，是更多箱子的sb哦
+						if(te.getClass().getSimpleName().contains("ShulkerBox")){
+							retHashCode = event.getContext().get(EventContextKeys.BLOCK_HIT).get().getLocation().get().getTileEntity().get().hashCode();
+						}
+					}
+				}
+			}
+		}
+		return retHashCode;
 	}
 }
